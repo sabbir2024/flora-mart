2729cc4239f399705a6c3a558e47265394334f2e
sabbir2024 <mdsabbirhossen2021@gmail.com>
2026-02-27 19:55:44 +0600

    most populor section added to home

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,10 +1,12 @@
 import Container from "../components/Container";
 import Banner from "./conponents/banner/Banner";
+import MostPopulor from "./conponents/most-populor/MostPopulor";
 
 export default function page() {
     return (
         <Container>
             <Banner />
+            <MostPopulor />
         </Container>
     );
 }
---
+++ app/components/Card.jsx
@@ -0,0 +1,20 @@
+export default function Card({ item }) {
+    return (
+        <div className="card card-sm bg-base-200 mx-w-60 shadow">
+
+            <figure className="hover-gallery">
+                <img src={item?.product_url} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title flex justify-between">
+                    {item?.product_name}
+                    <span className="flex gap-1 text-md font-bold w-full">
+                        <span className="flex"> {item?.price?.current_price} ৳ </span>
+                        <span className="text-red-700 flex line-through">{item?.price?.original_price} ৳ </span>
+                    </span>
+                </h2>
+                <div className="border-2 border-info text-center text-xl font-bold rounded-2xl">Order Now</div>
+            </div>
+        </div>
+    );
+}
