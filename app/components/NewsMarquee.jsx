21fe049f2b81f252043c755dc775f410ee435f75
sabbir2024 <mdsabbirhossen2021@gmail.com>
2026-03-11 11:29:26 +0600

    news option bg color chenge

--- app/components/NewsMarquee.jsx
+++ app/components/NewsMarquee.jsx
@@ -4,7 +4,7 @@ import Marquee from "react-fast-marquee";
 
 export default function NewsMarquee() {
     return (
-        <Marquee className="bg-blue-900 text-black py-2">
+        <Marquee className="font-bold text-black py-2">
             আপনার সকল সমস্যার সমাধানের জন্য আমাদের facebook page এর মেসেজ option এ গিয়ে মেসেজ করুন অথবা Call করুন ০১৬২৮৫০৭৮৩২ এই নাম্বারে।  ধন্যবাদ।         </Marquee>
     );
 }
