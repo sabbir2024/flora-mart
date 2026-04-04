415024630f8c9fce7f1ec5a2cf53a02fd8f622fb
sabbir2024 <mdsabbirhossen2021@gmail.com>
2026-04-04 10:43:40 +0600

    TopBar added to barnd name color black+orange

--- app/(root)/conponents/navbar/TopBar.jsx
+++ app/(root)/conponents/navbar/TopBar.jsx
@@ -11,7 +11,7 @@ export default function TopBar({ showCall }) {
         >
             <h1 className="font-bold text-white flex gap-1"> <FaPhone />
                 +880 1628-507832</h1>
-            <h1 className="flex text-md bg-white p-1 rounded-2xl font-bold">Fabric<span className="text-orange-600">Flora</span></h1>
+            <h1 className="flex text-md text-black bg-white p-1 rounded-2xl font-bold">Fabric<span className="text-orange-600">Flora</span></h1>
             <div className="flex justify-between gap-4">
                 <Link href={'https://www.facebook.com/people/Fabric-Flora-%E0%A6%AB%E0%A7%87%E0%A6%AC%E0%A7%8D%E0%A6%B0%E0%A6%BF%E0%A6%95-%E0%A6%AB%E0%A7%8D%E0%A6%B2%E0%A7%8B%E0%A6%B0%E0%A6%BE/61587371759656/'}>
                     <CgFacebook className="w-5 h-5 rounded-full text-orange-600 dark:text-black bg-amber-50 hover:bg-blue-600 hover:text-white transition-colors cursor-pointer" />
